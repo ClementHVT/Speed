e105896cd4400e6579a7dad0e5466da2c4ca1f03
Clément Hervouet <93199359+ClementHVT@users.noreply.github.com>
2026-02-27 09:28:07 +0000

    feat: Init project

---
+++ main.go
@@ -0,0 +1,15 @@
+package Speed
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/mem"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main(){
+	
+}
